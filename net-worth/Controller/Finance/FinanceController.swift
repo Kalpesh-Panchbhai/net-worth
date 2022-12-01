49b4dce8621265166f37555813891f92e6ab6f6b
Kalpesh-Panchbhai <kalpesh.panch90@gmail.com>
2022-12-01 19:13:27 +0530

    Removed unused code

--- net-worth/Controller/Finance/FinanceController.swift
+++ net-worth/Controller/Finance/FinanceController.swift
@@ -15,49 +15,6 @@ class FinanceController {
     
     private var financeDetailModel = FinanceDetailModel()
     
-//    public func getAllSymbols(searchTerm: String) -> [FinanceModel] {
-//
-//        dataCaptured = false
-//        var url = "https://query1.finance.yahoo.com/v1/finance/search?q=" + searchTerm
-//        url = url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-//        guard let url = URL(string: url) else {
-//            return financeModel
-//        }
-//
-//        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-//            guard let data = data, error == nil else {
-//                return
-//            }
-//
-//            guard let httpResponse = response as? HTTPURLResponse,
-//                  200 == httpResponse.statusCode else {
-//                return
-//            }
-//
-//            let decoder = JSONDecoder()
-//            decoder.keyDecodingStrategy = .convertFromSnakeCase
-//            let financeModelResponse = try! decoder.decode(FinanceModelResponse.self, from: data)
-//            self.financeModel = financeModelResponse.quotes.compactMap({
-//                var financeModel = FinanceModel()
-//                financeModel.shortname = $0.shortname
-//                financeModel.quoteType = $0.quoteType
-//                financeModel.symbol = $0.symbol
-//                financeModel.typeDisp = $0.typeDisp
-//                financeModel.longname = $0.longname
-//                financeModel.exchDisp = $0.exchDisp
-//
-//                return financeModel
-//            })
-//            self.dataCaptured = true
-//        }
-//
-//        task.resume()
-//
-//        while(!dataCaptured) {}
-//
-//        return financeModel
-//    }
-    
     public func getAllSymbols(searchTerm: String) async throws -> [FinanceModel] {
         
         dataCaptured = false
