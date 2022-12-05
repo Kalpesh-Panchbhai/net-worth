3da0e4a4277cf11216d5e2b5b6ff5be40af049eb
Kalpesh-Panchbhai <kalpesh.panch90@gmail.com>
2022-12-05 21:54:59 +0530

    Message Updated

--- net-worth/View/AccountView/AccountDetailsNavigationLinkView.swift
+++ net-worth/View/AccountView/AccountDetailsNavigationLinkView.swift
@@ -44,17 +44,9 @@ struct AccountDetailsNavigationLinkView: View {
             Spacer()
         }
         .toolbar {
-            if(account.accounttype == "Stock") {
+            if(account.accounttype == "Saving" || account.accounttype == "Credit Card" || account.accounttype == "Loan" || account.accounttype == "Other") {
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Add Shares", action: {
-                        self.isTransactionOpen.toggle()
-                    }).sheet(isPresented: $isTransactionOpen, content: {
-                        AddTransactionAccountView(account: self.account)
-                    })
-                }
-            } else if(account.accounttype == "Mutual Fund") {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Add Units", action: {
+                    Button("Add Transaction", action: {
                         self.isTransactionOpen.toggle()
                     }).sheet(isPresented: $isTransactionOpen, content: {
                         AddTransactionAccountView(account: self.account)
@@ -62,7 +54,7 @@ struct AccountDetailsNavigationLinkView: View {
                 }
             } else {
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Add Transaction", action: {
+                    Button("Add Units", action: {
                         self.isTransactionOpen.toggle()
                     }).sheet(isPresented: $isTransactionOpen, content: {
                         AddTransactionAccountView(account: self.account)
