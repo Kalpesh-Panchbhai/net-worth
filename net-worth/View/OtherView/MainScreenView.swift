086b4771b3bb32082b8ef408bf5f08d0477dea95
Kalpesh-Panchbhai <kalpesh.panch90@gmail.com>
2023-02-09 13:58:08 +0530

    Fixed Navigation View

--- net-worth/View/OtherView/MainScreenView.swift
+++ net-worth/View/OtherView/MainScreenView.swift
@@ -10,31 +10,33 @@ import SwiftUI
 struct MainScreenView: View {
     
     var body: some View {
-        TabView() {
-            AccountView()
-            .tabItem{
-                Image(systemName: "star.fill")
-                Text("Accounts")
-            }.tag("account_view")
-            IncomeView()
-            .tabItem{
-                Image(systemName: "indianrupeesign.circle.fill")
-                Text("Income")
-            }.tag("income_view")
-            SettingsView()
-            .tabItem{
-                Image(systemName: "slider.horizontal.3")
-                Text("Settings")
-            }.tag("settings_view")
-        }
-        .onAppear() {
-            UITabBar.appearance().barTintColor = .darkText
-            UITabBar.appearance().layer.cornerRadius = 25
-            UITabBar.appearance().clipsToBounds = true
-            UITabBar.appearance().layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        NavigationStack {
+            TabView() {
+                AccountView()
+                    .tabItem{
+                        Image(systemName: "star.fill")
+                        Text("Accounts")
+                    }.tag("account_view")
+                IncomeView()
+                    .tabItem{
+                        Image(systemName: "indianrupeesign.circle.fill")
+                        Text("Income")
+                    }.tag("income_view")
+                SettingsView()
+                    .tabItem{
+                        Image(systemName: "slider.horizontal.3")
+                        Text("Settings")
+                    }.tag("settings_view")
+            }
+            .onAppear() {
+                UITabBar.appearance().barTintColor = .darkText
+                UITabBar.appearance().layer.cornerRadius = 25
+                UITabBar.appearance().clipsToBounds = true
+                UITabBar.appearance().layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            }
         }
     }
-
+    
 }
 
 struct MainScreenView_Previews: PreviewProvider {
