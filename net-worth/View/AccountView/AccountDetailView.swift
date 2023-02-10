125bb85ba6b071cc12058ba10861fd4684652333
Kalpesh-Panchbhai <kalpesh.panch90@gmail.com>
2023-02-10 15:51:14 +0530

    Added Floating Button on Account Detail Page

---
+++ net-worth/View/AccountView/AccountDetailView.swift
@@ -0,0 +1,51 @@
+//
+//  AccountDetailView.swift
+//  net-worth
+//
+//  Created by Kalpesh Panchbhai on 10/02/23.
+//
+
+import SwiftUI
+
+struct AccountDetailView: View {
+    
+    private var account: Account
+    
+    @State private var show = false
+    
+    @StateObject var accountViewModel = AccountViewModel()
+    @StateObject var financeListViewModel = FinanceListViewModel()
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    init(account: Account) {
+        self.account = account
+    }
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                AccountDetailCardView(financeListViewModel: financeListViewModel, accountViewModel: accountViewModel)
+                    .cornerRadius(20)
+                    .shadow(color: Color.gray, radius: 3)
+                TransactionsView(accountViewModel: accountViewModel)
+            }
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    ExpendableButton(show: $show, accountViewModel: accountViewModel, financeListViewModel: financeListViewModel, presentationMode: _presentationMode, account: account)
+                }.padding([.bottom,.trailing],30)
+            }
+        }
+        .onAppear {
+            Task.init {
+                await financeListViewModel.getSymbolDetails(symbol: account.symbol)
+                await accountViewModel.getAccount(id: account.id!)
+                await accountViewModel.getAccountTransactionList(id: account.id!)
+                await accountViewModel.getLastTwoAccountTransactionList(id: account.id!)
+            }
+        }
+        .background(.black)
+    }
+}
