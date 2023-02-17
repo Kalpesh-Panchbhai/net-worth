038df19acb163e443a8c67c0ffa57d055b0a0794
Kalpesh-Panchbhai <kalpesh.panch90@gmail.com>
2023-02-17 18:56:19 +0530

    Removed floating menu

--- net-worth/View/AccountView/AccountDetailView.swift
+++ net-worth/View/AccountView/AccountDetailView.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct AccountDetailView: View {
     
+    private var dates = Array(1...28)
     private var account: Account
+    private var accountController = AccountController()
     
     @State private var show = false
+    @State var isNewTransactionViewOpen = false
+    @State var paymentDate = 0
     
     @ObservedObject var accountViewModel: AccountViewModel
     @StateObject var financeListViewModel = FinanceListViewModel()
@@ -31,23 +35,95 @@ struct AccountDetailView: View {
                     .shadow(color: Color.gray, radius: 3)
                 TransactionsView(accountViewModel: accountViewModel)
             }
-            VStack {
-                Spacer()
-                HStack {
-                    Spacer()
-                    AccountDetailExpendableButton(show: $show, accountViewModel: accountViewModel, financeListViewModel: financeListViewModel, presentationMode: _presentationMode, account: account)
-                }.padding([.trailing],30)
-                    .padding(.bottom,80)
-            }
+        }
+        .toolbar {
+            ToolbarItem(content: {
+                Menu(content: {
+                    Button(role: .destructive, action: {
+                        Task.init {
+                            try await accountController.deleteAccount(account: account)
+                        }
+                        self.presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        Label("Delete", systemImage: "trash")
+                    })
+                    
+                    Button(action: {
+                        self.isNewTransactionViewOpen.toggle()
+                    }, label: {
+                        Label("Add Transaction", systemImage: "square.and.pencil")
+                    })
+                    
+                    if(accountViewModel.account.accountType != "Saving") {
+                        if(!accountViewModel.account.paymentReminder) {
+                            Picker(selection: $paymentDate, content: {
+                                ForEach(dates, id: \.self) {
+                                    Text("\($0.formatted(.number.grouping(.never)))").tag($0)
+                                }
+                            }, label: {
+                                Label("Enable Notification", systemImage: "speaker.wave.1.fill")
+                            })
+                            .onChange(of: paymentDate) { _ in
+                                accountViewModel.account.paymentReminder = true
+                                accountViewModel.account.paymentDate = paymentDate
+                                accountController.updateAccount(account: accountViewModel.account)
+                                NotificationController().enableNotification(account: accountViewModel.account)
+                            }
+                            .pickerStyle(MenuPickerStyle())
+                        } else {
+                            Button(action: {
+                                accountViewModel.account.paymentReminder = false
+                                accountViewModel.account.paymentDate = 0
+                                accountController.updateAccount(account: accountViewModel.account)
+                                NotificationController().removeNotification(id: accountViewModel.account.id!)
+                                paymentDate = 0
+                            }, label: {
+                                Label("Disable Notification", systemImage: "speaker.slash.fill")
+                            })
+                            
+                            Picker(selection: $paymentDate, content: {
+                                ForEach(dates, id: \.self) {
+                                    Text("\($0.formatted(.number.grouping(.never)))").tag($0)
+                                }
+                            }, label: {
+                                Label("Change Date", systemImage: "calendar.circle.fill")
+                            })
+                            .onChange(of: paymentDate) { _ in
+                                accountViewModel.account.paymentReminder = true
+                                accountViewModel.account.paymentDate = paymentDate
+                                accountController.updateAccount(account: accountViewModel.account)
+                                NotificationController().enableNotification(account: accountViewModel.account)
+                            }
+                            .pickerStyle(MenuPickerStyle())
+                        }
+                    }
+                    
+                }, label: {
+                    Image(systemName: "ellipsis")
+                })
+            })
         }
         .onAppear {
             Task.init {
                 await financeListViewModel.getSymbolDetails(symbol: account.symbol)
                 await accountViewModel.getAccount(id: account.id!)
+                paymentDate = accountViewModel.account.paymentDate
                 await accountViewModel.getAccountTransactionList(id: account.id!)
                 await accountViewModel.getLastTwoAccountTransactionList(id: account.id!)
             }
         }
+        .sheet(isPresented: $isNewTransactionViewOpen, onDismiss: {
+            Task.init {
+                await accountViewModel.getAccount(id: accountViewModel.account.id!)
+                await accountViewModel.getAccountTransactionList(id: accountViewModel.account.id!)
+                await accountViewModel.getLastTwoAccountTransactionList(id: accountViewModel.account.id!)
+                await financeListViewModel.getSymbolDetails(symbol: accountViewModel.account.symbol)
+                await accountViewModel.getAccountList()
+                await accountViewModel.getTotalBalance(accountList: accountViewModel.accountList)
+            }
+        }, content: {
+            UpdateBalanceAccountView(accountViewModel: accountViewModel)
+        })
         .background(.black)
     }
 }
