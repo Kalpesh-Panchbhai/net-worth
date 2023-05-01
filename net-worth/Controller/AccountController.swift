d92cf3c3d0e4e95e76668e6d6725c729ae2dc622
Kalpesh-Panchbhai <kalpesh.panch90@gmail.com>
2023-05-01 11:54:07 +0530

    Fixed

--- net-worth/Controller/AccountController.swift
+++ net-worth/Controller/AccountController.swift
@@ -225,8 +225,11 @@ class AccountController {
                     }
                     let accountTransaction = try await self.getLastTwoAccountTransactionList(id: account.id!)
                     balanceModel.currentValue = balanceModel.currentValue * account.currentBalance
-                    if(accountTransaction.count > 1 && accountTransaction[1].timestamp.timeIntervalSince(Date()) > -86400) {
+                    if(accountTransaction.count > 1 && accountTransaction[0].timestamp.timeIntervalSince(Date()) > -86400) {
                         balanceModel.previousDayValue = balanceModel.previousDayValue * accountTransaction[1].balanceChange
+                    } else if(accountTransaction.count == 1 && accountTransaction[0].timestamp.timeIntervalSince(Date()) > -86400) {
+                        balanceModel.currentValue = balanceModel.previousDayValue * accountTransaction[0].balanceChange
+                        balanceModel.previousDayValue = 0
                     } else {
                         balanceModel.previousDayValue = balanceModel.previousDayValue * account.currentBalance
                     }
