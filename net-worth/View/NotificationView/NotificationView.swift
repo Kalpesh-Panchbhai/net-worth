3043db031ea16973c6f82cecf60e888f29d0cc65
Kalpesh-Panchbhai <kalpesh.panch90@gmail.com>
2023-05-02 15:03:44 +0530

    Fixed

--- net-worth/View/NotificationView/NotificationView.swift
+++ net-worth/View/NotificationView/NotificationView.swift
@@ -12,9 +12,3 @@ struct NotificationView: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
-
-struct NotificationView_Previews: PreviewProvider {
-    static var previews: some View {
-        NotificationView()
-    }
-}
