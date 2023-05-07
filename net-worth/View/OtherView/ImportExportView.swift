d68274030141ca676d28a49da8a30f89f955f153
Kalpesh-Panchbhai <kalpesh.panch90@gmail.com>
2023-05-07 21:29:37 +0530

    Added a empty Page for Backup

--- net-worth/View/OtherView/ImportExportView.swift
+++ net-worth/View/OtherView/ImportExportView.swift
@@ -15,76 +15,86 @@ struct ImportExportView: View {
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(0..<importExportViewModel.backupList.count, id: \.self, content: { i in
+            if(importExportViewModel.backupList.count == 0) {
+                ZStack {
                     HStack {
-                        if(i == 0) {
-                            
-                            Text(importExportViewModel.backupList[i].getDateAndFormat() + " at " + importExportViewModel.backupList[i].getTimeAndFormat())
-                            Spacer()
-                            Text("Latest")
-                                .foregroundColor(.green)
-                        } else {
-                            Text(importExportViewModel.backupList[i].getDateAndFormat() + " at " + importExportViewModel.backupList[i].getTimeAndFormat())
-                            
+                        Text("Click on ")
+                        Image(systemName: "tray.and.arrow.down.fill")
+                        Text(" Icon to backup data.")
+                    }
+                }
+            } else {
+                List {
+                    ForEach(0..<importExportViewModel.backupList.count, id: \.self, content: { i in
+                        HStack {
+                            if(i == 0) {
+                                
+                                Text(importExportViewModel.backupList[i].getDateAndFormat() + " at " + importExportViewModel.backupList[i].getTimeAndFormat())
+                                Spacer()
+                                Text("Latest")
+                                    .foregroundColor(.green)
+                            } else {
+                                Text(importExportViewModel.backupList[i].getDateAndFormat() + " at " + importExportViewModel.backupList[i].getTimeAndFormat())
+                                
+                            }
                         }
-                    }
-                    .contextMenu {
-                        Button(role: .destructive, action: {
-                            importExportController.deleteBackup(backupDate: importExportViewModel.backupList[i])
-                            Task.init {
-                                await importExportViewModel.getAllBackup()
-                            }
-                        }, label: {
-                            Label("Delete", systemImage: "trash")
-                        })
-                        
-                        Button(action: {
-                            let date = importExportViewModel.backupList[i]
-                            Task.init {
-                                await importExportController.importLocal(date: date, importType: "Account")
-                            }
-                        }, label: {
-                            Text("Import Accounts")
-                        })
-                        
-                        Button(action: {
-                            let date = importExportViewModel.backupList[i]
-                            Task.init {
-                                await importExportController.importLocal(date: date, importType: "Income")
-                            }
-                        }, label: {
-                            Text("Import Incomes")
-                        })
-                        
-                        Button(action: {
-                            let date = importExportViewModel.backupList[i]
-                            Task.init {
-                                await importExportController.importLocal(date: date, importType: "Tag")
-                            }
-                        }, label: {
-                            Text("Import Income tags")
-                        })
-                        
-                        Button(action: {
-                            let date = importExportViewModel.backupList[i]
-                            Task.init {
-                                await importExportController.importLocal(date: date, importType: "Type")
-                            }
-                        }, label: {
-                            Text("Import Income types")
-                        })
-                        
-                        Button(action: {
-                            let date = importExportViewModel.backupList[i]
-                            Task.init {
-                                await importExportController.importLocal(date: date, importType: "WatchList")
-                            }
-                        }, label: {
-                            Text("Import Watchlists")
-                        })
-                    }
-                })
+                        .contextMenu {
+                            Button(role: .destructive, action: {
+                                importExportController.deleteBackup(backupDate: importExportViewModel.backupList[i])
+                                Task.init {
+                                    await importExportViewModel.getAllBackup()
+                                }
+                            }, label: {
+                                Label("Delete", systemImage: "trash")
+                            })
+                            
+                            Button(action: {
+                                let date = importExportViewModel.backupList[i]
+                                Task.init {
+                                    await importExportController.importLocal(date: date, importType: "Account")
+                                }
+                            }, label: {
+                                Text("Import Accounts")
+                            })
+                            
+                            Button(action: {
+                                let date = importExportViewModel.backupList[i]
+                                Task.init {
+                                    await importExportController.importLocal(date: date, importType: "Income")
+                                }
+                            }, label: {
+                                Text("Import Incomes")
+                            })
+                            
+                            Button(action: {
+                                let date = importExportViewModel.backupList[i]
+                                Task.init {
+                                    await importExportController.importLocal(date: date, importType: "Tag")
+                                }
+                            }, label: {
+                                Text("Import Income tags")
+                            })
+                            
+                            Button(action: {
+                                let date = importExportViewModel.backupList[i]
+                                Task.init {
+                                    await importExportController.importLocal(date: date, importType: "Type")
+                                }
+                            }, label: {
+                                Text("Import Income types")
+                            })
+                            
+                            Button(action: {
+                                let date = importExportViewModel.backupList[i]
+                                Task.init {
+                                    await importExportController.importLocal(date: date, importType: "WatchList")
+                                }
+                            }, label: {
+                                Text("Import Watchlists")
+                            })
+                        }
+                    })
+                }
             }
         }
         .onAppear {
