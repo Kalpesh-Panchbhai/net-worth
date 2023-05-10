fbd6cd23fdbcddae1ec3c68a1eee8043756176f2
Kalpesh-Panchbhai <kalpesh.panch90@gmail.com>
2023-05-10 17:34:58 +0530

    Fixed

--- net-worth/View/IncomeView/IncomeDetailView.swift
+++ net-worth/View/IncomeView/IncomeDetailView.swift
@@ -19,32 +19,47 @@ struct IncomeDetailView: View {
                     Spacer()
                     Text(income.tag)
                 }
+                .foregroundColor(Color.navyBlue)
+                
                 HStack {
                     Text("Income Type")
                     Spacer()
                     Text(income.type)
                 }
+                .foregroundColor(Color.navyBlue)
+                
                 HStack {
                     Text("Amount")
                     Spacer()
                     Text("\(income.amount.withCommas(decimalPlace: 2))")
                 }
+                .foregroundColor(Color.navyBlue)
+                
                 HStack {
                     Text("Tax Paid")
                     Spacer()
                     Text("\(income.taxpaid.withCommas(decimalPlace: 2))")
                 }
+                .foregroundColor(Color.navyBlue)
+                
                 HStack {
                     Text("Credited On")
                     Spacer()
                     Text(income.creditedOn.getDateAndFormat())
                 }
+                .foregroundColor(Color.navyBlue)
+                
                 HStack {
                     Text("Currency")
                     Spacer()
                     Text(income.currency)
                 }
+                .foregroundColor(Color.navyBlue)
+                
             }
+            .listRowBackground(Color.white)
         }
+        .background(Color.navyBlue)
+        .scrollContentBackground(.hidden)
     }
 }
