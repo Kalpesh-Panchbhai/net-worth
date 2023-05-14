6896071992c4da285a9a268f11ecc1136b9951cc
Kalpesh Panchbhai <kalpesh.panch90@gmail.com>
2023-05-14 11:18:12 +0530

    Delete Pods directory

--- Pods/Target Support Files/Pods-net-worth-net-worthUITests/Pods-net-worth-net-worthUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_net_worth_net_worthUITests : NSObject
-@end
-@implementation PodsDummy_Pods_net_worth_net_worthUITests
-@end
--- Pods/Target Support Files/Pods-net-worthTests/Pods-net-worthTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_net_worthTests : NSObject
-@end
-@implementation PodsDummy_Pods_net_worthTests
-@end
