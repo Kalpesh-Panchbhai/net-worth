f85e6097c2a059dd2758deb1a7f55009a222a918
Kalpesh-Panchbhai <kalpesh.panch90@gmail.com>
2023-05-21 13:37:04 +0530

    Fixed

--- net-worth/View/OtherView/BackupView.swift
+++ net-worth/View/OtherView/BackupView.swift
@@ -10,57 +10,78 @@ import SwiftUI
 struct BackupView: View {
     
     private var importExportController = ImportExportController()
+    private var accountController = AccountController()
+    private var incomeController = IncomeController()
+    private var watchController = WatchController()
     
     @StateObject private var importExportViewModel = ImportExportViewModel()
+    
+    @State private var totalAccountInCloud = 0
+    @State private var totalAccountTransactionInCloud = 0
+    @State private var totalIncomeInCloud = 0
+    @State private var totalWatchListInCloud = 0
+    @State private var totalIncomeTypeInCloud = 0
+    @State private var totalIncomeTagInCloud = 0
     
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
         NavigationView {
-            if(importExportViewModel.backupList.isEmpty) {
-                ZStack {
-                    Color.navyBlue.ignoresSafeArea()
+            List {
+                VStack {
+                    Text("Data in Local Backup")
+                        .bold()
+                    Divider()
                     HStack {
-                        Text("Click on")
-                        Image(systemName: "tray.and.arrow.down.fill")
-                        Text("Icon to backup data.")
-                    }
-                    .foregroundColor(Color.lightBlue)
-                    .bold()
-                }
-            } else {
-                List {
-                    VStack {
-                        HStack {
-                            Image(systemName: "checkmark.icloud")
-                                .font(.system(size: 40))
-                            if(importExportViewModel.backupList.count > 0) {
-                                Text("Last Backup: " + importExportViewModel.backupList[0].getDateAndFormat() + ", " + importExportViewModel.backupList[0].getTimeAndFormat())
-                                    .font(.system(size: 12))
-                            } else {
-                                Text("Last Backup: -")
-                                    .font(.system(size: 12))
-                            }
+                        Image(systemName: "checkmark.icloud")
+                            .font(.system(size: 40))
+                        if(importExportViewModel.backupList.count > 0) {
+                            Text("Last Backup: " + importExportViewModel.backupList[0].getDateAndFormat() + ", " + importExportViewModel.backupList[0].getTimeAndFormat())
+                                .font(.system(size: 12))
+                        } else {
+                            Text("Last Backup: -")
+                                .font(.system(size: 12))
                         }
-                        Divider()
-                        account
-                        accountTransactions
-                        income
-                        watchList
-                        incomeTag
-                        incomeType
                     }
-                    .listRowBackground(Color.white)
-                    .foregroundColor(Color.navyBlue)
+                    Divider()
+                    account
+                    accountTransactions
+                    income
+                    watchList
+                    incomeTag
+                    incomeType
                 }
-                .background(Color.navyBlue)
-                .scrollContentBackground(.hidden)
+                .listRowBackground(Color.white)
+                .foregroundColor(Color.navyBlue)
+                
+                VStack {
+                    Text("Data in Cloud")
+                        .bold()
+                    Divider()
+                    accountInCloud
+                    accountTransactionInCloud
+                    incomeInCloud
+                    watchListInCloud
+                    incomeTagInCloud
+                    incomeTypeInCloud
+                }
+                .listRowBackground(Color.white)
+                .foregroundColor(Color.navyBlue)
             }
+            .background(Color.navyBlue)
+            .scrollContentBackground(.hidden)
         }
         .onAppear {
             Task.init {
                 await importExportViewModel.getLocalBackup()
                 await importExportViewModel.readLocalBackup()
+                
+                try await getTotalAccountInCloud()
+                try await getTotalAccountTransactionInCloud()
+                try await getTotalIncomeInCloud()
+                try await getTotalWatchListInCloud()
+                try await getTotalIncomeTagInCloud()
+                try await getTotalIncomeTypeInCloud()
             }
         }
         .toolbar {
@@ -93,17 +114,17 @@ struct BackupView: View {
         )
     }
     
-    var account: some View {
+    private var account: some View {
         HStack {
-            Text("Total Accounts")
+            Text("Accounts")
             Spacer()
             Text("\(importExportViewModel.backupData.account.count)")
         }
     }
     
-    var accountTransactions: some View {
+    private var accountTransactions: some View {
         HStack {
-            Text("Total Accounts Transactions")
+            Text("Accounts Transactions")
             Spacer()
             Text("\(getTotalTransaction())")
         }
@@ -118,35 +139,110 @@ struct BackupView: View {
         return totalTransactions
     }
     
-    var income: some View {
+    private var income: some View {
         HStack {
-            Text("Total Incomes")
+            Text("Incomes")
             Spacer()
             Text("\(importExportViewModel.backupData.income.count)")
         }
     }
     
-    var incomeType: some View {
+    private var watchList: some View {
         HStack {
-            Text("Total Income Type")
+            Text("WatchLists")
             Spacer()
-            Text("\(importExportViewModel.backupData.incomeType.count)")
+            Text("\(importExportViewModel.backupData.watch.count)")
         }
     }
     
-    var incomeTag: some View {
+    private var incomeTag: some View {
         HStack {
-            Text("Total Income Tag")
+            Text("Income Tag")
             Spacer()
             Text("\(importExportViewModel.backupData.incomeTag.count)")
         }
     }
     
-    var watchList: some View {
+    private var incomeType: some View {
         HStack {
-            Text("Total WatchLists")
+            Text("Income Type")
             Spacer()
-            Text("\(importExportViewModel.backupData.watch.count)")
+            Text("\(importExportViewModel.backupData.incomeType.count)")
         }
+    }
+    
+    private var accountInCloud: some View {
+        HStack {
+            Text("Accounts")
+            Spacer()
+            Text("\(totalAccountInCloud)")
+        }
+    }
+    
+    private func getTotalAccountInCloud() async throws {
+        totalAccountInCloud = try await accountController.getAccountList().count
+    }
+    
+    private var accountTransactionInCloud: some View {
+        HStack {
+            Text("Accounts Transactions")
+            Spacer()
+            Text("\(totalAccountTransactionInCloud)")
+        }
+    }
+    
+    private func getTotalAccountTransactionInCloud() async throws {
+        let accountlist = try await accountController.getAccountList()
+        for account in accountlist {
+            try await totalAccountTransactionInCloud += accountController.getAccountTransactionList(id: account.id!).count
+        }
+    }
+    
+    private var incomeInCloud: some View {
+        HStack {
+            Text("Incomes")
+            Spacer()
+            Text("\(totalIncomeInCloud)")
+        }
+    }
+    
+    private func getTotalIncomeInCloud() async throws {
+        totalIncomeInCloud = try await incomeController.getIncomeList().count
+    }
+    
+    private var watchListInCloud: some View {
+        HStack {
+            Text("WatchLists")
+            Spacer()
+            Text("\(totalWatchListInCloud)")
+        }
+    }
+    
+    private func getTotalWatchListInCloud() async throws {
+        totalWatchListInCloud = try await watchController.getAllWatchList().count
+    }
+    
+    private var incomeTypeInCloud: some View {
+        HStack {
+            Text("Income Type")
+            Spacer()
+            Text("\(totalIncomeTypeInCloud)")
+        }
+    }
+    
+    private func getTotalIncomeTypeInCloud() async throws {
+        totalIncomeTypeInCloud = try await incomeController.getIncomeTypeList().count
+    }
+    
+    private var incomeTagInCloud: some View {
+        HStack {
+            Text("Income Tag")
+            Spacer()
+            Text("\(totalIncomeTagInCloud)")
+        }
+    }
+    
+    private func getTotalIncomeTagInCloud() async throws {
+        totalIncomeTagInCloud = try await incomeController.getIncomeTagList().count
     }
 }
