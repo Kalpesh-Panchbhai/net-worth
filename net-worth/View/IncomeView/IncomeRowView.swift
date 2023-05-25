48c325fbabfa3a0303ae72442dcdb9bf139f9b03
Kalpesh-Panchbhai <kalpesh.panch90@gmail.com>
2023-05-25 20:32:38 +0530

    Refactored Income View

---
+++ net-worth/View/IncomeView/IncomeRowView.swift
@@ -0,0 +1,35 @@
+//
+//  IncomeRowView.swift
+//  net-worth
+//
+//  Created by Kalpesh Panchbhai on 25/05/23.
+//
+
+import SwiftUI
+
+struct IncomeRowView: View {
+    
+    var income: Income
+    
+    var body: some View {
+        HStack{
+            VStack {
+                Text(income.type)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text(income.creditedOn.getDateAndFormat()).font(.system(size: 12))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(Color.navyBlue.opacity(0.9))
+                Text(income.tag).font(.system(size: 10))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(Color.navyBlue.opacity(0.9))
+            }
+            VStack {
+                Text("\(income.currency) " + income.amount.withCommas(decimalPlace: 2))
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                Text("\(income.currency) \(income.avgAmount.withCommas(decimalPlace: 2))").font(.system(size: 12))
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .foregroundColor(Color.navyBlue.opacity(0.9))
+            }
+        }
+    }
+}
