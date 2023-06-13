2f495dc577ca28f938ef0e50efb9029aaeb984ad
Kalpesh-Panchbhai <kalpesh.panch90@gmail.com>
2023-06-13 10:34:35 +0530

    Fixed

--- net-worth/View/ChartView/NetWorthChartView.swift
+++ net-worth/View/ChartView/NetWorthChartView.swift
@@ -26,40 +26,35 @@ struct NetWorthChartView: View {
             VStack {
                 HStack {
                     List {
-                        Section {
-                            SingleLineLollipopChartView(chartDataList: chartViewModel.chartDataList, isPercentageChart: true)
-                        }
-                        .listRowBackground(Color.theme.foreground)
+                        SingleLineLollipopChartView(chartDataList: chartViewModel.chartDataList, isPercentageChart: true)
+                            .listRowBackground(Color.theme.foreground)
                         
-                        Section {
-                            Picker(selection: $range, content: {
-                                Text("1M").tag("1M")
-                                Text("3M").tag("3M")
-                                Text("6M").tag("6M")
-                                Text("1Y").tag("1Y")
-                                Text("2Y").tag("2Y")
-                                Text("5Y").tag("5Y")
-                                Text("All").tag("All")
-                            }, label: {
-                                
-                            })
-                            .onChange(of: range) { value in
-                                Task.init {
-                                    await incomeViewModel.getIncomeList()
-                                    await accountViewModel.getAccountTransactionListWithRangeMultipleAccounts(accountList: accountList, range: range)
-                                    if(!range.elementsEqual("All")) {
-                                        await accountViewModel.getAccountLastTransactionBelowRange(accountList: accountList, range: range)
-                                    }
-                                    await chartViewModel.getChartDataForAccounts(accountViewModel: accountViewModel, range: range)
-                                    await chartViewModel.getChartDataForNetworth(incomeViewModel: incomeViewModel)
+                        Picker(selection: $range, content: {
+                            Text("1M").tag("1M")
+                            Text("3M").tag("3M")
+                            Text("6M").tag("6M")
+                            Text("1Y").tag("1Y")
+                            Text("2Y").tag("2Y")
+                            Text("5Y").tag("5Y")
+                            Text("All").tag("All")
+                        }, label: {
+                            
+                        })
+                        .onChange(of: range) { value in
+                            Task.init {
+                                await incomeViewModel.getIncomeList()
+                                await accountViewModel.getAccountTransactionListWithRangeMultipleAccounts(accountList: accountList, range: range)
+                                if(!range.elementsEqual("All")) {
+                                    await accountViewModel.getAccountLastTransactionBelowRange(accountList: accountList, range: range)
                                 }
-                                let impact = UIImpactFeedbackGenerator(style: .light)
-                                impact.impactOccurred()
+                                await chartViewModel.getChartDataForAccounts(accountViewModel: accountViewModel, range: range)
+                                await chartViewModel.getChartDataForNetworth(incomeViewModel: incomeViewModel)
                             }
-                            .pickerStyle(SegmentedPickerStyle())
+                            let impact = UIImpactFeedbackGenerator(style: .light)
+                            impact.impactOccurred()
                         }
+                        .pickerStyle(SegmentedPickerStyle())
                         .listRowBackground(Color.theme.foreground)
-                        .foregroundColor(Color.theme.primaryText)
                     }
                     .background(Color.theme.background)
                     .scrollContentBackground(.hidden)
