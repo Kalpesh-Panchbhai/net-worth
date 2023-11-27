9d9488a266bf2fdd9b8a50c787cbd6d1d53e8703
Kalpesh Panchbhai <kalpesh.panchbhai@servicenow.com>
2023-11-27 17:17:42 +0530

    Fixed

---
+++ net-worth/Common/Model/FirebaseModel/GroupChartData.swift
@@ -0,0 +1,15 @@
+//
+//  GroupChartData.swift
+//  net-worth
+//
+//  Created by Kalpesh Panchbhai on 27/11/23.
+//
+
+import Foundation
+
+struct GroupChartData: Codable, Hashable {
+    
+    var groupType: String
+    
+    var value: Double
+}
--- net-worth/View/ChartView/Chart/BarLollipopGroupChartView.swift
+++ net-worth/View/ChartView/Chart/BarLollipopGroupChartView.swift
@@ -13,6 +13,7 @@ struct BarLollipopGroupChartView: View {
     var chartDataList: [String: Double]
     
     @State private var lineWidth = 2.0
+    @State private var selectedElement: GroupChartData?
     
     var body: some View {
         Chart {
@@ -49,7 +50,87 @@ struct BarLollipopGroupChartView: View {
                 .foregroundStyle(Color.theme.primaryText)
             }
         }
+        .chartOverlay { proxy in
+            GeometryReader { geo in
+                Rectangle().fill(.clear).contentShape(Rectangle())
+                    .gesture(
+                        SpatialTapGesture()
+                            .onEnded { value in
+                                let element = findElement(location: value.location, proxy: proxy, geometry: geo)
+                                if selectedElement?.groupType == element?.groupType {
+                                    // If tapping the same element, clear the selection.
+                                    selectedElement = nil
+                                } else {
+                                    selectedElement = element
+                                }
+                            }
+                            .exclusively(
+                                before: DragGesture()
+                                    .onChanged { value in
+                                        selectedElement = findElement(location: value.location, proxy: proxy, geometry: geo)
+                                    }
+                            )
+                    )
+            }
+        }
+        .chartBackground { proxy in
+            ZStack(alignment: .topLeading) {
+                GeometryReader { geo in
+                    if let selectedElement {
+                        let startPositionX1 = proxy.position(forX: selectedElement.groupType) ?? 0
+                        let startPositionY1 = proxy.position(forY: selectedElement.value) ?? 0
+                        
+                        let lineX = startPositionX1 + geo[proxy.plotAreaFrame].origin.x
+                        let lineY = startPositionY1 + geo[proxy.plotAreaFrame].origin.y
+                        
+                        let lineHeight = geo[proxy.plotAreaFrame].maxY
+                        let boxWidth: CGFloat = 100
+                        let boxOffset = max(0, min(geo.size.width - boxWidth, lineX - boxWidth / 2))
+                        
+                        Rectangle()
+                            .fill(Color.theme.green)
+                            .frame(width: 2, height: lineHeight)
+                            .position(x: lineX, y: lineHeight / 2)
+                        
+                        Circle()
+                            .strokeBorder(Color.theme.primaryText, lineWidth: 3)
+                            .background(Circle().fill(Color.theme.green))
+                            .frame(width: 12, height: 12)
+                            .position(x: lineX, y: lineY)
+                        
+                        VStack(alignment: .center) {
+                            Text(selectedElement.groupType)
+                                .font(.system(size: 10).bold())
+                                .foregroundColor(Color.theme.background)
+                            Text("\(selectedElement.value.withCommas(decimalPlace: 2))")
+                                .font(.system(size: 12).bold())
+                                .foregroundColor(Color.theme.background)
+                        }
+                        .accessibilityElement(children: .combine)
+                        .accessibilityHidden(false)
+                        .frame(width: boxWidth, alignment: .center)
+                        .background {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 8)
+                                    .fill(Color.theme.primaryText)
+                            }
+                            .padding(.horizontal, -8)
+                            .padding(.vertical, -4)
+                        }
+                        .offset(x: boxOffset)
+                    }
+                }
+            }
+        }
         .frame(height: 400)
     }
-
+    
+    private func findElement(location: CGPoint, proxy: ChartProxy, geometry: GeometryProxy) -> GroupChartData? {
+        let relativeXPosition = location.x - geometry[proxy.plotAreaFrame].origin.x
+        if let selectedGroup = proxy.value(atX: relativeXPosition) as String? {
+            return GroupChartData(groupType: selectedGroup, value: chartDataList[selectedGroup] ?? 0.0)
+        }
+        return nil
+    }
+    
 }
